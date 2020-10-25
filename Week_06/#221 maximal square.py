0f8f5701613fa87e3d0b3ce79ba32adefec4f2aa
joanneliu <joanne2301@hotmail.com>
2020-10-25 23:46:40 +0800

    week6 hw

---
+++ Week_06/#221 maximal square.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return 0
+
+        Max = 0
+        r, cols = len(matrix), len(matrix[0])
+        dp = [[0] * cols for _ in range(r)]
+        for i in range(r):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                    Max = max(Max, dp[i][j])
+
+        area = Max * Max
+        return area
